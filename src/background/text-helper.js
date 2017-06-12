429b69eefdde45dc302ab4ba923aa6c0865d0ebc
Joel Purra <code+github@joelpurra.com>
2017-06-12 17:03:05 +0200

    Only split text to paragraphs

--- src/background/text-helper.js
+++ src/background/text-helper.js
@@ -18,51 +18,9 @@ You should have received a copy of the GNU General Public License
 along with Talkie.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import {
-    last,
-} from "../shared/basic";
-
 export default class TextHelper {}
 
 TextHelper.splitTextToParagraphs = (text) => {
     // NOTE: in effect discarding empty paragraphs.
-    return text.split(/\n+/);
-};
-
-TextHelper.splitTextToSentencesOfMaxLength = (text, maxPartLength) => {
-    // NOTE: in effect merging multiple spaces in row to a single space.
-    const spacedTextParts = text.split(/ +/);
-
-    const naturalPauseRx = /(^--?$|[.,!?:;]$)/;
-
-    const textParts = spacedTextParts.reduce((newParts, spacedTextPart) => {
-        const appendToText = (ttt) => {
-            if (last(newParts) === "") {
-                newParts[newParts.length - 1] = ttt;
-            } else {
-                newParts[newParts.length - 1] += " " + ttt;
-            }
-        };
-
-        const appendPart = (ttt) => {
-            newParts[newParts.length] = ttt;
-        };
-
-        if (naturalPauseRx.test(spacedTextPart)) {
-            appendToText(spacedTextPart);
-
-            appendPart("");
-        } else if ((last(newParts).length + 1 + spacedTextPart.length) < maxPartLength) {
-            appendToText(spacedTextPart);
-        } else {
-            appendPart(spacedTextPart);
-        }
-
-        return newParts;
-    }, [""]);
-
-    // NOTE: cleaning empty strings "just in case".
-    const cleanTextParts = textParts.filter((textPart) => textPart.trim().length > 0);
-
-    return cleanTextParts;
+    return text.split(/[\n\r\u2028\u2029]+/);
 };
